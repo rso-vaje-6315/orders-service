2ed559854b11121b33edcad8185f53d8c40f4daf
Matej <mb7628@student.uni-lj.si>
2020-01-15 22:19:21 +0100

    dodano preverjanje za zaloge izdelkov pri kreiranju narocila

---
+++ services/src/main/java/si/rso/orders/restclients/StockApi.java
@@ -0,0 +1,16 @@
+package si.rso.orders.restclients;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@RegisterRestClient
+public interface StockApi {
+
+    @GET
+    @Path("/allstock/{productId}")
+    Long getNumberOfAllProducts(@PathParam("productId") String productId);
+}
